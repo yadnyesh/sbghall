a7afbc1568e15b943457ccab26b3235b874d4ffa
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-07-01 06:57:50 +0530

    Added

---
+++ src/main/java/io/yadnyesh/sbghall/SbghallApplication.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.sbghall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbghallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbghallApplication.class, args);
+	}
+
+}
